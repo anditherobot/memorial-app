e2305306a6aaec261cc308b96577e59ddae8e60f
Andi Milhomme <expandable@outlook.com>
2025-09-27 16:48:23 -0400

    chore(gitignore): ignore storage caches/logs/uploads and untrack compiled files

--- storage/framework/views/9f7f109159b9e6662bce27ebf15204fd.php
+++
@@ -1,3 +0,0 @@
-<div class="p-3 rounded bg-blue-50 text-blue-700">Thank you. Your wish is submitted and awaiting approval.</div>
-
-<?php /**PATH C:\Users\expan\Desktop\memorial\resources\views/wishes/_submitted.blade.php ENDPATH**/ ?>
